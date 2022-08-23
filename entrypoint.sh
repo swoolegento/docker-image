631f48537783a4ea1e153224429c066a6b59ac4b
Alex Ryall <alex@ryall.tk>
2022-08-23 19:34:58 +0100

    fix issue when php memory limit environment variable not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sed -i 's/memory_limit = 1G/memory_limit = '${PHP_MEMORY_LIMIT}'/' /usr/local/etc/php/php.ini
+if [ ! -z "$PHP_MEMORY_LIMIT" ]
+then
+    sed -i 's/memory_limit = 1G/memory_limit = '$PHP_MEMORY_LIMIT'/' /usr/local/etc/php/php.ini
+fi
 
 FILE=/var/www/html/app/etc/config.php
-if test ! -f "$FILE"; then
+if test ! -f "$FILE"
+then
     /bin/su -s /bin/bash -c '/home/docker/install.sh' docker
 else
     /bin/su -s /bin/bash -c 'php bin/magento setup:upgrade --keep-generated' docker
@@ -11,7 +15,7 @@ fi
 
 if [ "$SWOOLE_YASD_ENABLE" == "1" ] && ! grep -q "zend_extension=yasd" /usr/local/etc/php/conf.d/docker-php-ext-swoole.ini
 then
-    echo -e 'zend_extension=yasd\nyasd.debug_mode=remote\nyasd.remote_host='${SWOOLE_YASD_REMOTE_IP}'\nyasd.remote_port=9000' >> /usr/local/etc/php/conf.d/docker-php-ext-swoole.ini
+    echo -e 'zend_extension=yasd\nyasd.debug_mode=remote\nyasd.remote_host='$SWOOLE_YASD_REMOTE_IP'\nyasd.remote_port=9000' >> /usr/local/etc/php/conf.d/docker-php-ext-swoole.ini
 fi
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
